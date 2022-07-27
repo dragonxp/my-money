3b4458cf78f25d484ece547d41f29ca0bc2d727b
drogan xp <goodmsaul@gmail.com>
2022-07-27 20:41:33 +0545

    final commit project completion

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,68 @@
+import { useState, useEffect, useReducer } from "react"
+import { projectFirestore, timestamp } from '../firebase/config'
+
+const inititalState = {
+    isPending: false,
+    document: null,
+    success: null,
+    error: null
+}
+
+const firestoreReducer = (state, action) => {
+    switch (action.type) {
+        case 'IS_PENDING':
+            return { isPending: true, document: null, success: false, error: null }
+        case 'ADDED_DOCUMENT':
+            return { isPending: false, document: action.payload, success: true, error: null }
+        case 'DELETED_DOCUMENT':
+            return { isPending: false, document: null, success: true, error: null }
+        case 'ERROR':
+            return { isPending: false, document: null, success: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const useFirestore = (collection) => {
+    const [response, dispatch] = useReducer(firestoreReducer, inititalState)
+    const [isCancelled, setIsCancelled] = useState(false)
+
+    //firestore collection ref
+    const ref = projectFirestore.collection(collection)
+
+    //only dispatch if isCancelled = false
+    const dispatchIfNotCancelled = (action) => {
+        if (!isCancelled) dispatch(action)
+    }
+
+    //add a document
+    const addDocument = async (doc) => {
+        dispatchIfNotCancelled({ type: 'IS_PENDING' })
+
+        try {
+            const createdAt = timestamp.fromDate(new Date())
+            const addedDocument = await ref.add({ ...doc, createdAt })
+            dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
+        } catch (err) {
+            dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
+        }
+    }
+
+    //delete a document
+    const deleteDocument = async (id) => {
+        dispatchIfNotCancelled({ type: 'IS_PENDING' })
+
+        try {
+            await ref.doc(id).delete()
+            dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' })
+        } catch (err) {
+            dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
+        }
+    }
+
+    useEffect(() => {
+        return () => setIsCancelled(true)
+    }, [])
+
+    return { addDocument, deleteDocument, response }
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,31 @@
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useCollection } from '../../hooks/useCollection'
+
 //styles
 import styles from './Home.module.css'
 
+//components
+import TransactionForm from './TransactionForm'
+import TransactionList from './TransactionList'
+
 export default function Home() {
+	const { user } = useAuthContext()
+	const { documents, error, isPending } = useCollection(
+		'transactions',
+		["uid", "==", user.uid],
+		["createdAt", "desc"]
+	)
+
 	return (
-		<div>Home</div>
+		<div className={styles.container}>
+			<div className={styles.content}>
+				{isPending && <p>Loading...</p>}
+				{error && <p>{error}</p>}
+				{documents && <TransactionList transactions={documents} />}
+			</div>
+			<div className={styles.sidebar}>
+				<TransactionForm uid={user.uid} />
+			</div>
+		</div>
 	)
 }
